01dba1577680d394f005433f8f12becfc7f157d0
igor <igordot@users.noreply.github.com>
2016-07-20 12:14:21 -0400

    Create heatmaps-2016-07.R

---
+++ heatmaps-2016-07.R
@@ -0,0 +1,16 @@
+# creating heatmaps in R using gene expression data
+
+
+# prerequisites
+
+# install the relevant packages if they are not installed
+install.packages("RColorBrewer")
+install.packages("pheatmap")
+
+# download the data files
+download.file("https://raw.githubusercontent.com/igordot/tutorials/master/ca-genes-stats-sig.csv", destfile="ca-genes-stats-sig.csv")
+download.file("https://raw.githubusercontent.com/igordot/tutorials/master/ca-genes-fpkm.csv", destfile="ca-genes-fpkm.csv")
+
+# load the relevant packages
+library(RColorBrewer)
+library(pheatmap)
